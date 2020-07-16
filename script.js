27568fd93cf82bb5c626be4599e2a3a2385f6d78
Jerry Doran <Jerry.Doran@pccairfoils.com>
2020-07-16 10:57:26 -0400

    Added navabar, footer, and contact form sections

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const navButtons = Array.from(document.querySelectorAll('.nav-btn'));
+const navLinks = Array.from(document.querySelectorAll('.nav-link'));
+
+const colors = ['#6495ed', '#7fffd4', '#ffa07a', '#f08080', '#afeeee'];
+let i = 0;
 
 navButtons.forEach((btn) => {
   btn.onclick = () => {
     btn.parentElement.parentElement.classList.toggle('change');
   };
+});
+
+navLinks.forEach((link) => {
+  link.style.cssText = `background-color: ${colors[i]}`;
+  i += 1;
 });
