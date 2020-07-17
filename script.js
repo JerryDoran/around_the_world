93906c0338cac565fc021f59b95777d6f8474ff6
Jerry Doran <Jerry.Doran@pccairfoils.com>
2020-07-17 09:19:19 -0400

    Finished project

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const navButtons = Array.from(document.querySelectorAll('.nav-btn'));
 const navLinks = Array.from(document.querySelectorAll('.nav-link'));
+const container = document.querySelector('.container');
+const openMenuIcon = document.querySelector('.open-navbar-icon');
+const closeMenuIcon = document.querySelector('.close-navbar-icon');
 
 const colors = ['#6495ed', '#7fffd4', '#ffa07a', '#f08080', '#afeeee'];
 let i = 0;
@@ -13,4 +16,12 @@ navButtons.forEach((btn) => {
 navLinks.forEach((link) => {
   link.style.cssText = `background-color: ${colors[i]}`;
   i += 1;
+});
+
+openMenuIcon.addEventListener('click', () => {
+  container.classList.add('change');
+});
+
+closeMenuIcon.addEventListener('click', () => {
+  container.classList.remove('change');
 });
